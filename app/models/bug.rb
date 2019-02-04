9cd0e4257868bf11898af72bd460e01ceaf83e7c
degranjo <degranjo@mail.gvsu.edu>
2019-02-04 14:37:28 -0500

    Added user and relationship

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -1,4 +1,6 @@
 class Bug < ApplicationRecord
+  belongs_to :user
+  
   enum issue_type: [:issue, :enhancement, :feature]
   enum priority: [:low, :medium, :high]
   enum status: [:open, :closed, :monitor]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_03_210449) do
+ActiveRecord::Schema.define(version: 2019_02_04_193139) do
 
   create_table "bugs", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,17 @@ ActiveRecord::Schema.define(version: 2019_02_03_210449) do
     t.integer "issue_type"
     t.integer "priority"
     t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_bugs_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "lname"
+    t.string "fname"
+    t.string "email"
+    t.string "thumbnail"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
