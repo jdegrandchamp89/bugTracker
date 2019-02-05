024c4c4f43cf2d2eeeec3f63c2371e17f85040f4
degranjo <degranjo@mail.gvsu.edu>
2019-02-05 09:02:29 -0500

    finish adding user to bug views and controller

--- app/controllers/bugs_controller.rb
+++ app/controllers/bugs_controller.rb
@@ -28,6 +28,8 @@ class BugsController < ApplicationController
   # POST /bugs.json
   def create
     @bug = Bug.new(bug_params)
+    user = User.find(bug_params[:user_id])
+    @bug.build_user(:id => user.id)
 
     respond_to do |format|
       if @bug.save
@@ -72,7 +74,7 @@ class BugsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def bug_params
-      params.require(:bug).permit(:title, :description, :issue_type, :priority, :status)
+      params.require(:bug).permit(:title, :description, :issue_type, :priority, :status, :user_id)
     end
     
     def set_issue_type
